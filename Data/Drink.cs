40e950420a11cd321fc9c37599e08f508f8e101e
itsttrucne <ntttruccv@gmail.com>
2025-11-09 18:34:56 +0700

    Add files via upload

---
+++ Data/Drink.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebBanNuocMVC.Data;
+
+public partial class Drink
+{
+    public int DrinkId { get; set; }
+
+    public string? DrinkName { get; set; }
+
+    public int? CategoryId { get; set; }
+
+    public decimal? Price { get; set; }
+
+    public string? Description { get; set; }
+
+    public string? Image { get; set; }
+
+    public virtual Category? Category { get; set; }
+
+    public virtual ICollection<OrderDetail> OrderDetails { get; set; } = new List<OrderDetail>();
+}
